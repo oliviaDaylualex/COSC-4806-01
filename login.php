b0716cae5f8c758fa0a0cfc915d6f4ecbc1ce4ee
oliviaDaylualex <oliviaalex799@gmail.com>
2025-07-16 22:58:35 +0000

    updated login page

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+  </head>
+     <body>
+
+    <h1>Login Form</h1>
+
+
+    </body>
+<form action="/validate.php" method ="post">
+  <label for="username">Username:</label>
+  <br>
+  <input type="text" id="username" name="username" >
+  <br> 
+  <label for="password"></label>Password:</label>
+  <br>
+  <input type="password" id="password" name="password" >
+  <br><br>
+  <input type="submit" value="Submit">
+</form>
