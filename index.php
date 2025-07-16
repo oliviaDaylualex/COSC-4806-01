0710c81d0934cfb5563134049c3a7fb98c6af6c2
oliviaDaylualex <oliviaalex799@gmail.com>
2025-07-16 22:57:01 +0000

    Updated index page

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
+<?php
+session_start();
+if (!isset($_SESSION['authenticated'])) {
+header('Location: /login.php');
+exit();
+}
+?>
+<!DOCTYPE html>
 <html>
-  <head>
-    <title>PHP Test</title>
+<head>
+<title>Olivia</title>
   </head>
-  <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+     <body>
+
+    <h1>Assignment 1</h1>
+
+
+
+
+        <p> Welcome , <?=$_SESSION['username']?> </p>
+
+        <p>Today is <?=date('l, F j, Y')?></p>
+
+
+    </body>
+
+   <footer>
+        <p><a href="/logout.php">Logout</a></p>
+    </footer>
 
 </html>
