0a9cea7789c3efb323454c0519d0159a9badbf5a
oliviaDaylualex <oliviaalex799@gmail.com>
2025-07-16 23:01:35 +0000

    updated validate page with username and password

--- validate.php
+++ validate.php
@@ -3,9 +3,14 @@
 
 session_start();
 
-$valid_username = 'Aswin';
+$valid_username = 'Olivia';
 $valid_password = '12345';
 
 $username = $_REQUEST['username'];
 $_SESSION['username'] = $username;
 $password = $_REQUEST['password'];
+
+if ($username == $valid_username && $password == $valid_password){
+$_SESSION['authenticated'] = 1;
+header('Location: /index.php');
+echo "Login successful";
