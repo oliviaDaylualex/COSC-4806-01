2d346ce960ebf9f9d2811457c2fb771db36aa5f8
oliviaDaylualex <oliviaalex799@gmail.com>
2025-07-16 23:02:29 +0000

    added conditions to validate page

--- validate.php
+++ validate.php
@@ -14,3 +14,16 @@ if ($username == $valid_username && $password == $valid_password){
 $_SESSION['authenticated'] = 1;
 header('Location: /index.php');
 echo "Login successful";
+
+}else {
+    if(!isset($_SESSION['login_attempts'])){
+      $_SESSION['login_attempts'] = 1;
+    }else{
+      $_SESSION['login_attempts'] = $_SESSION['login_attempts'] + 1;;
+    }
+    echo "Login failed. Attempts: " . $_SESSION['login_attempts'];
+
+  }
+
+
+  ?>
